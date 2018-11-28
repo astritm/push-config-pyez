c7bbfa7c8265222a1bf50b854e019573406e7008
astritm <astrit.morina@gmail.com>
2018-11-28 10:51:45 +0100

    removing the file

--- PUSH-config-change-using-PyEZ-master/push-config-change-pyez.py
+++
@@ -1,8 +0,0 @@
-from jnpr.junos import Device
-dev = Device('192.168.0.1')
-from jnpr.junos.utils.config import Config
-cu = Config(dev)
-dev.open()
-rsp = cu.load( path="config-example.conf" )
-cu.commit()
-
